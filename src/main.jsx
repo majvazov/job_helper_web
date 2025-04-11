199d5809426755b525ce237149dae1f6bca1d92b
Martin Ayvazov <martin.ayvazov@gmail.com>
2025-04-11 22:58:44 +0300

    removing unused data import

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { importInterviewData } from './utils/importData';
-
-// Import interview data to Firestore (will only import if data doesn't exist)
-importInterviewData();
+// filepath: /home/martin/Development/job_helper_web/src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // Or your main App component
+import './index.css' // Or your main CSS file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
